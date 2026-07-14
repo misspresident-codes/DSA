59bcb84047688b0859f5394c4f8c030b6396f297
Khyati Sahu <skhyati263@gmail.com>
2026-07-15 00:04:23 +0530

    Time: 0 ms (100.00%) | Memory: 14.9 MB (37.81%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -4,24 +4,23 @@
 class Solution {
 public:
     std::vector<int> twoSum(std::vector<int>& nums, int target) {
-        // Map to store: Key = number, Value = its index
-        std::unordered_map<int, int> num_to_index;
+        // Map to store: Key = number value, Value = its index
+        std::unordered_map<int, int> numMap;
         
-        for (int i = 0; i < nums.size(); ++i) {
+        for (int i = 0; i < nums.size(); i++) {
             int complement = target - nums[i];
             
-            // Check if the complement already exists in the map
-            if (num_to_index.find(complement) != num_to_index.end()) {
-                // If found, return the index of the complement and the current index
-                return {num_to_index[complement], i};
+            // Check if the complement exists in our map
+            if (numMap.find(complement) != numMap.end()) {
+                // If found, return the index of complement and current index
+                return {numMap[complement], i};
             }
             
-            // Otherwise, store the current number and its index in the map
-            num_to_index[nums[i]] = i;
+            // Otherwise, insert the current number and index into the map
+            numMap[nums[i]] = i;
         }
         
-        // Return an empty vector if no solution is found (guaranteed not to happen per constraints)
+        // Return empty vector if no solution is found (guaranteed not to hit per constraints)
         return {};
     }
 };
-
